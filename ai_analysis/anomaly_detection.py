aa911d9f86e8132d9bc1df60aef7f208b9e169c1
Priya2191-dev <lalepriyanka@gmail.com>
2026-03-11 19:47:39 +0530

    Update anomaly_detection.py

--- ai_analysis/anomaly_detection.py
+++ ai_analysis/anomaly_detection.py
@@ -1,1 +1,12 @@
+import numpy as np
 
+def detect_anomaly(speed_values):
+
+    mean_speed = np.mean(speed_values)
+
+    for speed in speed_values:
+
+        if abs(speed - mean_speed) > 30:
+            return True
+
+    return False
