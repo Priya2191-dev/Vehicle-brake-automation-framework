e40cde2616b18dbe20382c253955bd8b47230608
Priya2191-dev <lalepriyanka@gmail.com>
2026-03-11 18:40:36 +0530

    Update can_simulator.py

--- can_bus/can_simulator.py
+++ can_bus/can_simulator.py
@@ -1,1 +1,13 @@
+import can
 
+bus = can.interface.Bus(channel="virtual", bustype="virtual")
+
+message = can.Message(
+    arbitration_id=0x101,
+    data=[int(vehicle.speed), vehicle.brake_pressure, int(vehicle.abs_active)],
+    is_extended_id=False
+)
+
+bus.send(message)
+
+print("CAN Message Sent:", message)
