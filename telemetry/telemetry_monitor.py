349ed7fa4565ed96ec408a89c6bd9e8b55305e1b
Priya2191-dev <lalepriyanka@gmail.com>
2026-03-11 18:30:08 +0530

    Update telemetry_monitor.py

--- telemetry/telemetry_monitor.py
+++ telemetry/telemetry_monitor.py
@@ -1,1 +1,18 @@
+class TelemetryMonitor:
 
+    def __init__(self):
+        self.logs = []
+
+    def record(self, speed, pressure):
+
+        data = {
+            "speed": speed,
+            "brake_pressure": pressure
+        }
+
+        self.logs.append(data)
+
+    def display(self):
+
+        for log in self.logs:
+            print(log)
