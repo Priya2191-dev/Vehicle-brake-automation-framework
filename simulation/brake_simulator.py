92899eb766cc22618d578570f76860f37d743e47
Priya2191-dev <lalepriyanka@gmail.com>
2026-03-11 18:31:43 +0530

    Update brake_simulator.py

--- simulation/brake_simulator.py
+++ simulation/brake_simulator.py
@@ -1,1 +1,14 @@
+class BrakeSimulator:
 
+    def __init__(self, speed):
+        self.speed = speed
+
+    def apply_brake(self, pressure):
+
+        deceleration = pressure * 0.15
+        self.speed -= deceleration
+
+        if self.speed < 0:
+            self.speed = 0
+
+        return self.speed
