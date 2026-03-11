b1b14619410e1ce14ea9746f562413a0ca4f390d
Priya2191-dev <lalepriyanka@gmail.com>
2026-03-11 18:33:23 +0530

    Update vehicle_model.py

--- digital_twin/vehicle_model.py
+++ digital_twin/vehicle_model.py
@@ -1,1 +1,19 @@
+class Vehicle:
 
+    def __init__(self, mass=1500):
+        self.mass = mass
+        self.speed = 0
+        self.brake_pressure = 0
+
+    def accelerate(self, value):
+        self.speed += value
+
+    def apply_brake(self, pressure):
+
+        self.brake_pressure = pressure
+        self.speed -= pressure * 0.2
+
+        if self.speed < 0:
+            self.speed = 0
+
+        return self.speed
