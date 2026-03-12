73d6dba3f46d4a22d2405f06f463265d9e378608
Priya2191-dev <lalepriyanka@gmail.com>
2026-03-12 14:54:22 +0530

    Update brake_steps.py

--- step_definitions/brake_steps.py
+++ step_definitions/brake_steps.py
@@ -1,9 +1,21 @@
+from behave import given, when, then
 from simulation.brake_simulator import BrakeSimulator
 
-def test_brake():
 
-    sim = BrakeSimulator(100)
+@given('vehicle speed is {speed:d} km/h')
+def step_vehicle_speed(context, speed):
 
-    new_speed = sim.apply_brake(20)
+    context.simulator = BrakeSimulator(speed)
+    context.initial_speed = speed
 
-    assert new_speed < 100
+
+@when('brake pedal is pressed with pressure {pressure:d}')
+def step_apply_brake(context, pressure):
+
+    context.new_speed = context.simulator.apply_brake(pressure)
+
+
+@then('vehicle speed should decrease')
+def step_verify_speed(context):
+
+    assert context.new_speed < context.initial_speed
