6f332f0214ca7b637898b325b8984cadbdb16bb7
Priya2191-dev <lalepriyanka@gmail.com>
2026-03-12 14:05:40 +0530

    Update brake_dashboard.py

--- dashboard/brake_dashboard.py
+++ dashboard/brake_dashboard.py
@@ -1,1 +1,12 @@
+import matplotlib.pyplot as plt
 
+def plot_speed(speed_data):
+
+    plt.plot(speed_data)
+
+    plt.title("Vehicle Speed During Braking")
+
+    plt.xlabel("Time")
+    plt.ylabel("Speed")
+
+    plt.show()
