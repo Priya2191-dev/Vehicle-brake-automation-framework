4fc4ff75e240a1db887ba24573aba56ec9c2f239
Priya2191-dev <lalepriyanka@gmail.com>
2026-03-12 14:56:39 +0530

    Update abs_steps.py

--- step_definitions/abs_steps.py
+++ step_definitions/abs_steps.py
@@ -1,1 +1,31 @@
+from behave import given, when, then
 
+
+@given('vehicle speed is {speed:d} km/h')
+def step_speed(context, speed):
+
+    context.speed = speed
+
+
+@when('brake pedal is pressed suddenly')
+def step_sudden_brake(context):
+
+    context.abs_active = True
+
+
+@then('ABS system should activate')
+def step_verify_abs(context):
+
+    assert context.abs_active == True
+
+
+@when('sudden brake is applied')
+def step_apply(context):
+
+    context.wheel_locked = False
+
+
+@then('wheels should not lock')
+def step_verify(context):
+
+    assert context.wheel_locked == False
