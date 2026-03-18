081b746693ab014d88e464b145e9982dc3b370bb
Priya2191-dev <lalepriyanka@gmail.com>
2026-03-19 00:25:04 +0530

    Create vehicle_steps.py

---
+++ features/steps/vehicle_steps.py
@@ -0,0 +1,27 @@
+from behave import given, when, then
+from vehicle import Vehicle  
+
+@given('a vehicle')
+def step_impl(context):
+    context.vehicle = Vehicle()
+
+@given('a vehicle with initial speed {speed}')
+def step_impl(context, speed):
+    context.vehicle = Vehicle()
+    context.vehicle.speed = float(speed)
+
+@when('the vehicle accelerates by {value}')
+def step_impl(context, value):
+    context.vehicle.accelerate(float(value))
+
+@when('brake is applied with pressure {pressure}')
+def step_impl(context, pressure):
+    context.new_speed = context.vehicle.apply_brake(float(pressure))
+
+@then('the speed should be {expected}')
+def step_impl(context, expected):
+    assert round(context.vehicle.speed, 2) == float(expected)
+
+@then('brake pressure should be {pressure}')
+def step_impl(context, pressure):
+    assert context.vehicle.brake_pressure == float(pressure)
