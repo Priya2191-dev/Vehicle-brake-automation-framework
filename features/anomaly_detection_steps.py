dd3f18742e24d24f28fa5454722157f84cbc22b4
Priya2191-dev <lalepriyanka@gmail.com>
2026-03-18 19:23:57 +0530

    Create anomaly_detection_steps.py

---
+++ features/anomaly_detection_steps.py
@@ -0,0 +1,18 @@
+from behave import given, when, then
+from anomaly_detection import detect_anomaly  
+
+@given('speed values {values}')
+def step_impl(context, values):
+    context.speeds = [int(v.strip()) for v in values.split(",")]
+
+@when('anomaly detection is performed')
+def step_impl(context):
+    context.result = detect_anomaly(context.speeds)
+
+@then('anomaly should be detected')
+def step_impl(context):
+    assert context.result is True
+
+@then('no anomaly should be detected')
+def step_impl(context):
+    assert context.result is False
