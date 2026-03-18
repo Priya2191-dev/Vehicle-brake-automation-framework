968df5481468a4becf020eb917cc6e7d5772239f
Priya2191-dev <lalepriyanka@gmail.com>
2026-03-18 19:20:06 +0530

    Update anomaly_detection.py

--- src/anomaly_detection.py
+++ src/anomaly_detection.py
@@ -2,4 +2,4 @@ import numpy as np
 
 def detect_anomaly(speed_values):
     mean = np.mean(speed_values)
-    return any(abs(v - mean) > 30 for v in speed_value)
+    return any(abs(v - mean) > 30 for v in speed_values)
