a8fe476b66464fc4ad91baf29697769bf31bd1d9
Priya2191-dev <lalepriyanka@gmail.com>
2026-03-21 15:41:24 +0530

    Update brake_simulator_steps.py

--- features/steps/brake_simulator_steps.py
+++ features/steps/brake_simulator_steps.py
@@ -2,13 +2,13 @@ from behave import given, when, then
 from brake_simulator import BrakeSimulator   
 
 @given('a vehicle with speed {speed}')
-def step_impl(context, speed):
+def given_vehicle_speed(context, speed):
     context.simulator = BrakeSimulator(float(speed))
 
 @when('brake is applied with pressure {pressure}')
-def step_impl(context, pressure):
+def when_apply_brake(context, pressure):
     context.new_speed = context.simulator.apply_brake(float(pressure))
 
 @then('the new speed should be {expected}')
-def step_impl(context, expected):
+def then_verify_speed(context, expected):
     assert round(context.new_speed, 2) == float(expected)
