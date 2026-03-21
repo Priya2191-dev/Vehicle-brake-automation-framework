b86ab3e4be77a36e34daa2aad37857298d7e799d
Priya2191-dev <lalepriyanka@gmail.com>
2026-03-21 13:34:33 +0530

    Update brake_steps.py

--- features/steps/brake_steps.py
+++ features/steps/brake_steps.py
@@ -2,17 +2,17 @@ from behave import given, when, then
 from src.braking import automated_braking
 
 @given('vehicle speed is 80')
-def step(context):
+def given_speed(context):
     context.speed = 80
 
 @given('obstacle distance is 5')
-def step(context):
+def given_distance(context):
     context.distance = 5
 
 @when('system evaluates braking')
-def step(context):
+def when_evaluate(context):
     context.pressure = automated_braking(context.speed, context.distance)
 
 @then('brake pressure should be 40')
-def step(context):
+def then_verify_pressure(context):
     assert context.pressure == 40
