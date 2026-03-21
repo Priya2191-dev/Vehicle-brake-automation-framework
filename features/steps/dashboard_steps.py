9bf78db786db0326d1c136f4710133e4296b1847
Priya2191-dev <lalepriyanka@gmail.com>
2026-03-21 15:52:46 +0530

    Update dashboard_steps.py

--- features/steps/dashboard_steps.py
+++ features/steps/dashboard_steps.py
@@ -3,17 +3,17 @@ import os
 from dashboard import plot
 
 @given('dashboard speed values {values}')
-def step_impl(context, values):
+def given_speed_values(context, values):
     context.speed = [int(v) for v in values.split(",")]
 
 @given('dashboard pressure values {values}')
-def step_impl(context, values):
+def given_pressure_values(context, values):
     context.pressure = [int(v) for v in values.split(",")]
 
 @when('the plot is generated')
-def step_impl(context):
+def when_generate_plot(context):
     plot(context.speed, context.pressure)
 
 @then('a plot file should be created')
-def step_impl(context):
+def then_verify_plot(context):
     assert os.path.exists("plot.png")
