2de5f407d448c231383c77aec295fd2542975314
Priya2191-dev <lalepriyanka@gmail.com>
2026-03-23 09:59:23 +0000

    Update test_digital_twin.py

--- tests/test_digital_twin.py
+++ tests/test_digital_twin.py
@@ -4,4 +4,10 @@ def test_braking():
     v = Vehicle()
     v.accelerate(100)
     speed = v.apply_brake(20)
-    assert speed < 100
+    assert speed == 96
+
+def test_brake_not_negative():
+    v = Vehicle()
+    v.accelerate(10)
+    speed = v.apply_brake(100)
+    assert speed == 0
