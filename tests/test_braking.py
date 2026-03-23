15e398bdd97f297b1a64c473b6446fa0cc20b378
Priya2191-dev <lalepriyanka@gmail.com>
2026-03-23 12:21:57 +0000

    Update test_braking.py

--- tests/test_braking.py
+++ tests/test_braking.py
@@ -2,3 +2,9 @@ from braking import automated_braking
 
 def test_auto_brake():
     assert automated_braking(80, 5) == 40
+
+def test_no_brake_when_safe_distance():
+    assert automated_braking(80, 20) == 0
+
+def test_no_brake_when_low_speed():
+    assert automated_braking(20, 5) == 0
