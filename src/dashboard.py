fb357ca652d3ed0fc363e0ebaab1b8033fa9b686
Priya2191-dev <lalepriyanka@gmail.com>
2026-03-30 13:15:43 +0530

    Update dashboard.py

--- src/dashboard.py
+++ src/dashboard.py
@@ -1,7 +1,19 @@
 import matplotlib.pyplot as plt
 
 def plot(speed, pressure):
-    plt.plot(speed)
-    plt.plot(pressure)
+    if len(speed) != len(pressure):
+        raise ValueError("Speed and pressure must have same length")
+
+    plt.figure()
+
+    plt.plot(speed, label="Speed")
+    plt.plot(pressure, label="Brake Pressure")
+
+    plt.title("Vehicle Brake Dashboard")
+    plt.xlabel("Time")
+    plt.ylabel("Values")
+
+    plt.legend()
+
     plt.savefig("plot.png")
     plt.close()
