376ab1b39c4762f08da0f325b925b96d7ccaa087
Priya2191-dev <lalepriyanka@gmail.com>
2026-03-30 13:18:28 +0530

    Update dashboard_steps.py

--- features/steps/dashboard_steps.py
+++ features/steps/dashboard_steps.py
@@ -3,17 +3,38 @@ import os
 from dashboard import plot
 
 @given('dashboard speed values {values}')
-def given_speed_values(context, values):
-    context.speed = [int(v) for v in values.split(",")]
+def given_speed(context, values=None):
+    if values:
+        context.speed = [int(v.strip()) for v in values.split(",")]
+    else:
+        context.speed = []
 
 @given('dashboard pressure values {values}')
-def given_pressure_values(context, values):
-    context.pressure = [int(v) for v in values.split(",")]
+def given_pressure(context, values=None):
+    if values:
+        context.pressure = [int(v.strip()) for v in values.split(",")]
+    else:
+        context.pressure = []
+
 
 @when('the plot is generated')
-def when_generate_plot(context):
+def when_plot(context):
     plot(context.speed, context.pressure)
 
+
+@when('the plot is generated with invalid data')
+def when_invalid_plot(context):
+    try:
+        plot(context.speed, context.pressure)
+    except ValueError as e:
+        context.error = e
+
+
 @then('a plot file should be created')
-def then_verify_plot(context):
+def then_file_created(context):
     assert os.path.exists("plot.png")
+
+
+@then('an error should be raised')
+def then_error(context):
+    assert isinstance(context.error, ValueError)
