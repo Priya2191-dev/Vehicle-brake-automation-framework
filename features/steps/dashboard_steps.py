477435eaf203c7ef1dfc686accdcca1f8d3bc67e
Priya2191-dev <lalepriyanka@gmail.com>
2026-03-30 13:29:14 +0530

    Update dashboard_steps.py

--- features/steps/dashboard_steps.py
+++ features/steps/dashboard_steps.py
@@ -4,23 +4,24 @@ from dashboard import plot
 
 @given('dashboard speed values {values}')
 def given_speed(context, values=None):
-    if values:
-        context.speed = [int(v.strip()) for v in values.split(",")]
-    else:
-        context.speed = []
-
+    context.speed = [int(v.strip()) for v in values.split(",")]
+    
 @given('dashboard pressure values {values}')
 def given_pressure(context, values=None):
-    if values:
-        context.pressure = [int(v.strip()) for v in values.split(",")]
-    else:
-        context.pressure = []
+    context.pressure = [int(v.strip()) for v in values.split(",")]
+    
+@given('dashboard speed values')
+def given_empty_speed(context):
+    context.speed = []
+
+@given('dashboard pressure values')
+def given_empty_pressure(context):
+    context.pressure = []
 
 
 @when('the plot is generated')
 def when_plot(context):
     plot(context.speed, context.pressure)
-
 
 @when('the plot is generated with invalid data')
 def when_invalid_plot(context):
@@ -33,7 +34,6 @@ def when_invalid_plot(context):
 @then('a plot file should be created')
 def then_file_created(context):
     assert os.path.exists("plot.png")
-
 
 @then('an dashboard error should be raised')
 def then_dashboard_error(context):
